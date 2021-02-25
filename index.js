321cdc0cf19b9aad549cc1f7bc0e8b8c5be82ba4
Jaira Arcilla <arcilla.jaira@gmail.com>
2021-02-24 20:46:45 -0800

    Got moment.js to work

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ $(document).ready (function() {
     var today = 
         moment().format('MMMM Do YYYY');
         console.log(today)
-        $(".todaysDate").append(today);
+        $("#currentDay").append(today);
     
     })
+
+$(".saveBtn").click(function(event) {
+    console.log(event.target);
+    console.log($(this).parent().siblings(".text-input").val());
+    const inputVal = $(this).parent().siblings(".text-input").val();
+    const btnKey = $(this).attr("id");
+    localStorage.setItem(btnKey, inputVal);
+})
+
+for (var i = 0; i < localStorage.length; i++) {
+    const keyName = localStorage.key(i);
+    const valContent = localStorage.getItem(localStorage.key(i));
+    
+    console.log(localStorage.getItem(localStorage.key(i)));
+    console.log(localStorage.key(i));
+    $("#" + keyName).parent().siblings(".text-input").val(valContent);
+}
